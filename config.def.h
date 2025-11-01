ed1ff135786b1dc3adda1b3324ea648b349cef01
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-01 01:33:36 +0000

    disabled anti-alias

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "TerminessNerdFont-Regular:pixelsize=15:antialias=true:autohint=true";
+static char *font = "TerminessNerdFont-Regular:pixelsize=16:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Noto Color Emoji:style=Regular:pixelsize=18:antialias=true:autohint=true",
+	"Noto Color Emoji:style=Regular:pixelsize=16:autohint=true",
 };
 static int borderpx = 15;
 
