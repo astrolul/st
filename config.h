ad40f68d93da8d30f0962fb124d662ab403f883b
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-12 14:35:21 +0000

    Reduce font pixel size from 30 to 25

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:style=Regular:pixelsize=30:autohint=true";
+static char *font = "FiraCode Nerd Font:style=Regular:pixelsize=25:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Noto Color Emoji:style=Regular:pixelsize=30:autohint=true",
+	"Noto Color Emoji:style=Regular:pixelsize=25:autohint=true",
 };
 static int borderpx = 20;
 
